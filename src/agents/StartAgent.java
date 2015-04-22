bc8b271404b194b179f684b071518bd86f61973e
Victor dos Santos <vitocr_santos@hotmail.com>
2015-04-22 19:44:17 -0300

    StartAgent v0.1

--- src/agents/StartAgent.java
+++ src/agents/StartAgent.java
@@ -1,10 +1,12 @@
 package agents;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+import behaviours.ManagerBehaviour;
 import jade.core.Agent;
 import jade.core.Runtime;
 import jade.core.Profile;
@@ -16,58 +18,7 @@ import jade.wrapper.StaleProxyException;
 @SuppressWarnings("serial")
 public class StartAgent extends Agent {
 
-//	private void combinations(int n)
-//	{
-//		Calcula as combinações de 1..n
-//		int s[] = new int[n];
-//		for (int i = 0; i < n; i++)
-//			s[i] = i+1;
-//		
-//		List<String> results = new ArrayList<String>();
-//		for ( int i = 1; i <= (1<<(s.length))-1; i++ ) 
-//		{
-//	      StringBuilder builder = new StringBuilder();
-//	      for ( int j = 0; j < s.length; j++ ) 
-//	      {
-//	        if ( (i & (1<<j)) != 0) 
-//	        {
-//	          builder.append(s[j]);
-//	        }
-//	      }
-//	      results.add(builder.toString());
-//	    }
-//	    System.out.println( results );
-//	    ---------------------------------------
-//		Junta os resultados em conjuntos que possuem todos os elementos
-//	    ArrayList<int[]> comb = new ArrayList<int[]>();
-//	    for (int i = results.size()-2; i > results.size() / 2; i--)
-//	    {
-//	    	System.out.println(results.get(i) + " | " + results.get(results.size() - i - 2));
-//	    }
-//		---------------------------------------
-//	}
-//	
-//	Map<String, Double> characteristicFunction = new HashMap<String, Double>();
-//	
-//	public double calculateCoallitionValue(double agentValue, double coallitionValue)
-//	{
-//		return -1;
-//	}
-//	
-//	public double coallitionValue(int agent, int[] coallition)
-//	{
-//		String query = Arrays.toString(coallition).replace(", ", "").replace("]","").substring(1) + Integer.toString(agent);
-//		if (characteristicFunction.containsKey(query))
-//			return characteristicFunction.get(query);
-//		else
-//		{
-//			double v = 0;
-//			
-//			characteristicFunction.put(query, v);
-//		}
-//		return -1;
-//		
-//	}
+	ArrayList<String> coalitions;
 	
 	private double[] initialValues(int n, double v)
 	{
@@ -96,11 +47,13 @@ public class StartAgent extends Agent {
 	
 	protected void setup()
 	{
+		int agents;
+		coalitions = new ArrayList<String>();
 		Object[] args = getArguments();
 		if (args != null && args.length > 0)
 		{
 //			Numero de Agentes
-			int agents = Integer.parseInt((String) args[0]);
+			agents = Integer.parseInt((String) args[0]);
 			double value = 100;
 			//caracteristicFunction(agents,value);
 			double a[] = initialValues(agents, value);
@@ -111,14 +64,24 @@ public class StartAgent extends Agent {
 //			Criando agentes
 			for (int i = 0; i < agents; i++)
 			{
+				coalitions.add(String.valueOf(i+1));
 				try {
-					AgentController ac = agentContainer.createNewAgent("CoalitionAgent"+(i+1), "agents.CoalitionAgent", new Object[] { Double.valueOf(a[i]), agents });
+					AgentController ac = agentContainer.createNewAgent("CoalitionAgent"+(i+1), "agents.CoalitionAgent", new Object[] { Double.valueOf(a[i]), agents, this });
 					ac.start();
 				} catch (StaleProxyException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-			}			
+			}
+			
+			this.addBehaviour(new ManagerBehaviour(this,agents));
 		}
-	}	
+	}
+
+	public void updateCoalitions(String coalition1, String coalition2)
+	{
+		coalitions.remove(coalition1);
+		coalitions.remove(coalition2);
+		coalitions.add(coalition1+coalition2);
+	}
 }
